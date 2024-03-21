55ac003786c346bf24e0090b217f213838a342d3
Max Mason <maxdoylemason@gmail.com>
2024-03-21 17:43:56 -0600

    create project structure

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,3 @@
+const Accordion = () => {};
+
+export default Accordion;
---
+++ src/pages/CareersPage/Careers.tsx
@@ -0,0 +1,6 @@
+import Accordion from "../../components/Accordion";
+import Carousel from "../../components/Carousel";
+
+const Careers = () => {};
+
+export default Careers;
